02d5655d12b4e3b17874630922ace1168a117094
Kasdan Bakos <kbakos@andrew.cmu.edu>
2019-11-02 14:24:46 -0400

    initial commit

---
+++ recipeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  recipeApp
+//
+//  Created by Kasdan on 10/31/19.
+//  Copyright © 2019 CMU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
