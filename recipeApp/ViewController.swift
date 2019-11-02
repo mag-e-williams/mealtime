45139a2d4e6ea89ea061106211d0788a52a81149
Kasdan Bakos <kbakos@andrew.cmu.edu>
2019-11-02 15:47:04 -0400

    Input text creates url string

--- recipeApp/ViewController.swift
+++ recipeApp/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ingredientInput: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func createQuery(sender: UIButton){
+        let queryArray = (ingredientInput.text!).split(separator: ",")
+        let interpString = queryArray.joined(separator: ",+")
+        let url = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=\(interpString)&apiKey=0ff5861766ea48b0a55b2008c47bd778"
+        
+        print(url)
     }
 
 
