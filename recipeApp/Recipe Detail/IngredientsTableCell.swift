a9a24bf5f3e7f3c0f68b6c16c00c2147e9dccb19
Margret Williams <margretw@andrew.cmu.edu>
2019-11-07 18:23:15 -0500

    recipes table

---
+++ recipeApp/Recipe Detail/IngredientsTableCell.swift
@@ -0,0 +1,25 @@
+//
+//  IngredientsTableCell.swift
+//  recipeApp
+//
+//  Created by Maggie Williams on 11/7/19.
+//  Copyright © 2019 CMU. All rights reserved.
+//
+
+import UIKit
+
+class IngredientsTableCell: UITableViewCell {
+  
+  @IBOutlet weak var title: UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    // Configure the view for the selected state
+  }
+  
+}
--- recipeApp/Recipe Detail/RecipeDetailViewController.swift
+++ recipeApp/Recipe Detail/RecipeDetailViewController.swift
@@ -8,39 +8,66 @@
 
 import UIKit
 
-class RecipeDetailViewController: UIViewController {
+class RecipeDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
   
   var recipeDetail: RecipeDetail?
   
   @IBOutlet var recipeLabel: UILabel!
   @IBOutlet weak var recipeImg: UIImageView!
+  
   @IBOutlet var ingredientsTable: UITableView!
+  @IBOutlet var instructionsTable: UITableView!
+
 
   var viewModel: RecipeDetailViewModel?
   var recipeID: Int?
   
   override func viewDidLoad() {
     super.viewDidLoad()
-  }
-  
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
     
+    let bundle = Bundle(for: type(of: self))
+    let cellNib = UINib(nibName: "IngredientsTableCell", bundle: bundle)
+    self.ingredientsTable.register(cellNib, forCellReuseIdentifier: "cell")
+  
     if let viewModel = viewModel {
       viewModel.refresh {
         self.recipeDetail = viewModel.recipeDetail
         self.loadDetails()
       }
     }
-    loadDetails()
   }
-    
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+   
+  }
+  
+//  loading all the data into the screen
   
   func loadDetails(){
     self.recipeLabel.text = self.recipeDetail?.title
     let imageURL = URL(string: self.recipeDetail!.image)
     self.recipeImg.downloadImage(from: imageURL!)
+    self.ingredientsTable.reloadData()
   }
+  
+  
+//  Ingredients table view
+  
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return (viewModel?.numberOfIngredientsTableRows()!)!
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! IngredientsTableCell
+    cell.title?.text = viewModel?.ingredientTitleForRowAtIndexPath(indexPath)
+    return cell
+  }
+
+  
+  
+//   Image Loading Functions
   
   func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
     URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
