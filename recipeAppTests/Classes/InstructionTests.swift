241fc0e7d88fe6df4afbd60b7f502b1485e588ff
Kasdan Bakos <kbakos@andrew.cmu.edu>
2019-11-07 21:54:03 -0500

    Added instruction test file

---
+++ recipeAppTests/Classes/InstructionTests.swift
@@ -0,0 +1,20 @@
+//
+//  InstructionTests.swift
+//  recipeAppTests
+//
+//  Created by Kasdan on 11/7/19.
+//  Copyright © 2019 CMU. All rights reserved.
+//
+
+import XCTest
+
+class InstructionTests: XCTestCase {
+    
+    
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
