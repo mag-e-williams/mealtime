3dbbd463428cc43f9f542cb338f5e0de25dbfaaf
Kasdan Bakos <kbakos@andrew.cmu.edu>
2019-12-05 02:16:01 -0500

    wrote core data for user

---
+++ User+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  User+CoreDataClass.swift
+//  
+//
+//  Created by Kasdan on 12/5/19.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
---
+++ User+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  User+CoreDataProperties.swift
+//  
+//
+//  Created by Kasdan on 12/5/19.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension User {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<User> {
+        return NSFetchRequest<User>(entityName: "User")
+    }
+
+    @NSManaged public var dietary_restrictions: String?
+    @NSManaged public var email: String?
+    @NSManaged public var first_name: String?
+    @NSManaged public var last_name: String?
+    @NSManaged public var preferences: String?
+    @NSManaged public var saved_recipes: NSObject?
+
+}
