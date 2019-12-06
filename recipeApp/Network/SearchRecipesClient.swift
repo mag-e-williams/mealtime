b013c9e94a7169bacd189be422035ed751c82050
Kasdan Bakos <kbakos@andrew.cmu.edu>
2019-12-06 02:06:32 -0500

    basing

--- recipeApp/Network/SearchRecipesClient.swift
+++ recipeApp/Network/SearchRecipesClient.swift
@@ -17,8 +17,7 @@ class SearchRecipesClient {
     let interpString = (cleanQuery).replacingOccurrences(of: " ", with: ",+")
     
     let url = "https://api.spoonacular.com/recipes/complexSearch?query=\(interpString)&number=10&apiKey=0ff5861766ea48b0a55b2008c47bd778"
-    print("url being used")
-    print(url)
+
     let recipes = getRecipes(url)
     
     completion(recipes)
