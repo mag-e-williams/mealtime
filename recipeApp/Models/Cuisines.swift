af1b504c7d00a2843e56c6966254a9c910749e79
Margret Williams <margretw@andrew.cmu.edu>
2019-12-05 19:08:43 -0500

    kaz for u

--- recipeApp/Models/Cuisines.swift
+++ recipeApp/Models/Cuisines.swift
@@ -6,36 +6,42 @@
 //  Copyright © 2019 CMU. All rights reserved.
 //
 
+
 import UIKit
 
-struct Cuisines {
-  
-  let cuisines = ["African",
-                  "American",
-                  "British",
-                  "Cajun",
-                  "Caribbean",
-                  "Chinese",
-                  "Eastern European",
-                  "European",
-                  "French",
-                  "German",
-                  "Greek",
-                  "Indian",
-                  "Irish",
-                  "Italian",
-                  "Japanese",
-                  "Jewish",
-                  "Korean",
-                  "Latin American",
-                  "Mediterranean",
-                  "Mexican",
-                  "Middle Eastern",
-                  "Nordic",
-                  "Southern",
-                  "Spanish",
-                  "Thai",
-                  "Vietnamese"
-                  ]
+struct Cuisine: Codable {
+  let title: String
+}
+
+struct Cuisines: Codable {
+  let cuisines = [
+  Cuisine(title: "African"),
+  Cuisine(title: "American"),
+  Cuisine(title: "British"),
+  Cuisine(title: "Cajun"),
+  Cuisine(title: "Caribbean"),
+  Cuisine(title: "Chinese"),
+  Cuisine(title: "Eastern European"),
+  Cuisine(title: "European"),
+  Cuisine(title: "French"),
+  Cuisine(title: "German"),
+  Cuisine(title: "Greek"),
+  Cuisine(title: "Indian"),
+  Cuisine(title: "Irish"),
+  Cuisine(title: "Italian"),
+  Cuisine(title: "Japanese"),
+  Cuisine(title: "Jewish"),
+  Cuisine(title: "Korean"),
+  Cuisine(title: "Latin American"),
+  Cuisine(title: "Mediterranean"),
+  Cuisine(title: "Mexican"),
+  Cuisine(title: "Middle Eastern"),
+  Cuisine(title: "Nordic"),
+  Cuisine(title: "Southern"),
+  Cuisine(title: "Spanish"),
+  Cuisine(title: "Thai"),
+  Cuisine(title: "Vietnamese"),
+  ]
   
 }
+
---
+++ recipeApp/Profile/EditProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  EditProfileViewController.swift
+//  recipeApp
+//
+//  Created by Maggie Williams on 12/5/19.
+//  Copyright © 2019 CMU. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class EditProfileViewController: UIViewController, UITextFieldDelegate {
+
+
+}
